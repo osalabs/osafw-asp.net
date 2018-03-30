ce6db6313ab6e97604dc6c2cb794f7f8e4ff5796
Oleg Savchuk <osalabs+github@gmail.com>
2018-03-30 15:55:25 -0500

    login fix

--- www/App_Data/template/login/index/onload.js
+++ www/App_Data/template/login/index/onload.js
@@ -1,7 +1,7 @@
 $(document).on('click', '.on-pwd-hideshow', pwd_hideshow);
 pwd_hideshow();
 
-//$('#login').focus();
+// $('#login').focus()
 
 $(document).on('keyup change', '.form-label-group input', input_on_change);
 $('.form-label-group input').trigger('change');
@@ -9,7 +9,18 @@ $('.form-label-group input').trigger('change');
 
 function input_on_change(e) {
   var $this=$(this);
-  if ($this.val()>''){
+  var is_filled= ($this.val()>'');
+  if (!is_filled){
+    //detect browser autofill
+    //try/catch for non-webkit
+    try {
+      is_filled = $this.is(":-webkit-autofill")
+    }catch(e){
+      // console.log(e)
+    }
+  }
+
+  if (is_filled){
       if (!$this.is('.filled')) $this.addClass('filled');
   }else{
       $this.removeClass('filled');
