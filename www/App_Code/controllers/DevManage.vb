8753bd44c9135cb0d329a90430ef28d83eef6caf
Oleg <oleg@engineeredit.com>
2018-12-28 11:18:24 -0600

    Added "Dump Log" to Developer Tools

--- www/App_Code/controllers/DevManage.vb
+++ www/App_Code/controllers/DevManage.vb
@@ -3,6 +3,8 @@
 ' Part of ASP.NET osa framework  www.osalabs.com/osafw/asp.net
 ' (c) 2009-2018  Oleg Savchuk www.osalabs.com
 
+Imports System.IO
+
 Public Class DevManageController
     Inherits FwController
     Public Shared Shadows access_level As Integer = 100
@@ -12,5 +14,22 @@ Public Class DevManageController
 
         Return ps
     End Function
+
+    Public Function DumpLogAction() As Hashtable
+        Dim seek = reqi("seek")
+        Dim logpath = fw.config("log")
+        rw("Dump of last " & seek & " bytes of the site log")
+
+        Dim fs = New FileStream(logpath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)
+        fs.Seek(-seek, SeekOrigin.End)
+        Dim sr = New StreamReader(fs)
+        rw("<pre>")
+        fw.resp.Write(sr.ReadToEnd())
+        rw("</pre>")
+
+        rw("end of dump")
+        sr.Close()
+    End Function
+
 
 End Class
